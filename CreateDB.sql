db4100bd8762a51e492d3004ae14a923481c4b6b
AlvinJ666 <51864834+AlvinJ666@users.noreply.github.com>
2021-06-09 15:55:24 -0400

    Create CreateDB.sql

---
+++ CreateDB.sql
@@ -0,0 +1,69 @@
+﻿/****** Object:  Table [dbo].[BlogPosts]******/
+
+CREATE TABLE [dbo].[BlogPosts](
+	[BlogPostId] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[Title] [nvarchar](200) NOT NULL,
+	[Content] [nvarchar](4000) NOT NULL,
+	[Posted] [datetime] NOT NULL,
+)
+
+GO
+
+CREATE TABLE [dbo].[Comments](
+	[CommentId] [int] IDENTITY(1,1) NOT NULL,
+	[BlogPostId] [int] NOT NULL,
+	[UserId] [int] NOT NULL,
+	[Content] [nvarchar](2048) NOT NULL,
+)
+
+GO
+/****** Object:  Table [dbo].[Roles]******/
+
+CREATE TABLE [dbo].[Roles](
+	[RoleId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+)
+
+GO
+/****** Object:  Table [dbo].[Users]******/
+
+CREATE TABLE [dbo].[Users](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [int] NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[EmailAddress] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+)
+
+GO
+SET IDENTITY_INSERT [dbo].[Roles] ON 
+
+INSERT [dbo].[Roles] ([RoleId], [Name]) VALUES (1, N'General User')
+INSERT [dbo].[Roles] ([RoleId], [Name]) VALUES (2, N'Blog Poster')
+SET IDENTITY_INSERT [dbo].[Roles] OFF
+ALTER TABLE [dbo].[BlogPosts]  WITH CHECK ADD  CONSTRAINT [FK_BlogPosts_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
+GO
+ALTER TABLE [dbo].[BlogPosts] CHECK CONSTRAINT [FK_BlogPosts_Users]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_BlogPosts] FOREIGN KEY([BlogPostId])
+REFERENCES [dbo].[BlogPosts] ([BlogPostId])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_BlogPosts]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Users]
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_Roles] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Roles] ([RoleId])
+GO
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_Roles]
+GO
+USE [master]
+GO
+ALTER DATABASE [Assignment1] SET  READ_WRITE 
+GO
