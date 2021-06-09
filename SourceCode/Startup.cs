0ad82afd10787005aabe57a50f1272002c3fcfe8
AlvinJ666 <51864834+AlvinJ666@users.noreply.github.com>
2021-06-09 16:31:31 -0400

    Add files via upload

---
+++ SourceCode/Startup.cs
@@ -0,0 +1,51 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Lab4_Cs.Models;
+using Azure.Storage.Blobs;
+
+namespace Lab4_Cs
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = @"Server=tcp:cs-server666.database.windows.net,1433;Initial Catalog=Lab6;Persist Security Info=False;User ID=alvin;Password=Jdx0316.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<Assigment1DbContext>(options => options.UseSqlServer(connection));
+
+            var blobConnection = @"DefaultEndpointsProtocol=https;AccountName=blog666;AccountKey=YiX8gAUYj/cOfwnXQXfUCUULfNgMrcm9I1mpJhui/WZji47li8XlbkzmzlSLZkM51OiQYAt5Y1j7AG09/VJp0g==;EndpointSuffix=core.windows.net";
+            services.AddSingleton(new BlobServiceClient(blobConnection));
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                   //template: "{controller=Shared}/{action=_Layout.cshtml}/{id?}");
+            });
+        }
+    }
+}
